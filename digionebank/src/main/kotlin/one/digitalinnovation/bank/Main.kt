d53d2a84f861809a4d746bd72c52aaa468b7766c
gabriel torres <gabrieltggv29@gmail.com>
2022-06-14 23:15:26 -0300

    Criado as classes Conta e Banco
      Na criação da classe Banco foi aplicado o conceito de data class.

---
+++ digionebank/src/main/kotlin/one/digitalinnovation/bank/Main.kt
@@ -0,0 +1,12 @@
+
+package one.digitalinnovation.bank
+
+
+fun main(){
+    val gabriel = Pessoa()
+    println(gabriel.nome)
+    println(gabriel.cpf)
+    println(gabriel.Endereco().rua)
+    Animal(tipo="Mamífero")
+
+}
