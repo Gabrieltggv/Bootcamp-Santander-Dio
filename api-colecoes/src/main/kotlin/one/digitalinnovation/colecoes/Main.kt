9aaa8b7897200fde182cc7aaced68c802a574094
gabriel torres <gabrieltggv29@gmail.com>
2022-06-16 08:29:03 -0300

    Criado arrays Double e de Strings

--- api-colecoes/src/main/kotlin/one/digitalinnovation/colecoes/Main.kt
+++ api-colecoes/src/main/kotlin/one/digitalinnovation/colecoes/Main.kt
@@ -16,4 +16,37 @@ fun main() {
     lista.forEach{ elemento ->
         println(elemento)
     }
+
+    "IntArrayOf".also(:: println)
+
+    val inteiros = intArrayOf(1, 2, 7, 8, 10)
+    inteiros.sort()
+    println(Arrays.toString(inteiros))
+
+    "Array de string".also(:: println)
+
+    val nomes = Array(3){""} // {""} esta inferindo o tipo
+    nomes[0] = "Elefante"
+    nomes[1] = "Abelha"
+    nomes[2] = "Cachorro"
+
+    val animais = arrayOf("Galinha", "tubarão")
+
+
+    animais.forEachIndexed { index, ele ->
+        "nome[$index] = $ele".also(:: println)
+    }
+
+    "DoubleArray".also(:: println)
+
+    val salarios = DoubleArray(3)
+    salarios[0] = 3000.0
+    salarios[1] = 400.0
+    salarios[2] = 500.0
+
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario + 4.5
+        println(salarios[index])
+    }
+
 }
