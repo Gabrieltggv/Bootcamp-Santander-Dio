aab974bdf00db89e1ecb5f7929997ad626a09cb2
gabriel torres <gabrieltggv29@gmail.com>
2022-06-16 12:11:48 -0300

    Refatorado com as funções maxOrNull e minOrNull

--- api-colecoes/src/main/kotlin/one/digitalinnovation/colecoes/Funcoes.kt
+++ api-colecoes/src/main/kotlin/one/digitalinnovation/colecoes/Funcoes.kt
@@ -4,20 +4,20 @@ import java.util.Arrays
 
 fun doubleOperacoes() {
     val salarios = doubleArrayOf(2000.0, 5000.0, 1500.0)
-    salarios.sort()
-    val minimo = salarios.first()
-    val maximo = salarios.last()
+    val minimo = salarios.minOrNull()
+    val maximo = salarios.maxOrNull()
     val media = salarios.average()
     val salariosPar = salarios.filter{ it < media }
 
-    println("Valor maximo: $minimo")
+    println("Valor minimo: $minimo")
 
     println("Valor maximo: $maximo")
-    println("Valor maximo: $media")
+    println("Valor media: $media")
     println(salariosPar)
 
+
     println(salarios.count{it % 3 == 0.0})
-    println(salarios.find{ it in minimo..media})
+    println(salarios.find{ it in minimo!!.rangeTo(media) })
     println(salarios.find{it == 500.0}) // return null
     println(salarios.any{it == 1500.0}) // return False
     println(salarios.any{it == 300.0}) // return False
