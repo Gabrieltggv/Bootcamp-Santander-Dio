9c32436b8f28cadb76e23c1bf0e41cf4d0063123
gabriel torres <gabrieltggv29@gmail.com>
2022-06-16 07:30:08 -0300

    Criação de um array
      Criado uma exemplo de criação de um array, possibilidades de
      interações.

--- api-colecoes/src/main/kotlin/one/digitalinnovation/colecoes/Main.kt
+++ api-colecoes/src/main/kotlin/one/digitalinnovation/colecoes/Main.kt
@@ -1,4 +1,19 @@
 package one.digitalinnovation.colecoes
 
+import java.util.Arrays
+
 fun main() {
+    val lista = IntArray(5)
+    var cont = 0
+
+    for (valor in lista) {
+        lista[cont] = cont + 1
+        cont ++
+    }
+
+    println(Arrays.toString(lista))
+
+    lista.forEach{ elemento ->
+        println(elemento)
+    }
 }
